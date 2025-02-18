9602fa35085cf1df0714e15e009a149c193e7420
Eloiza <eloiza.souza@zup.com.br>
2025-02-18 16:35:52 -0300

    chore: complete SecurityConfig class

--- src/main/java/br/com/zup/user_manager/user_manager/infra/security/SecurityConfig.java
+++ src/main/java/br/com/zup/user_manager/user_manager/infra/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.zup.user_manager.user_manager.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,17 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request ->
-                request.anyRequest().authenticated()
+                request.requestMatchers(HttpMethod.GET, "/user").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/user").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/user/login").permitAll()
+                        .anyRequest().authenticated()
         );
         http.csrf(AbstractHttpConfigurer::disable);
-
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
