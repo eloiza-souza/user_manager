f8ca41e34ca09bf08550a31419edf2f615910bc1
Eloiza <eloiza.souza@zup.com.br>
2025-02-21 10:01:49 -0300

    feat: add class Role

---
+++ src/main/java/br/com/zup/user_manager/user_manager/models/Role.java
@@ -0,0 +1,27 @@
+package br.com.zup.user_manager.user_manager.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
+}
