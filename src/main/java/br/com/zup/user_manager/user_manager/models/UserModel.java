411f6bc2e30ae9d92cdec1fc6070e83785f8bdaa
Eloiza <eloiza.souza@zup.com.br>
2025-02-21 10:03:25 -0300

    feat: include lombok and attribute Role to UserModel

--- src/main/java/br/com/zup/user_manager/user_manager/models/UserModel.java
+++ src/main/java/br/com/zup/user_manager/user_manager/models/UserModel.java
@@ -1,45 +1,37 @@
 package br.com.zup.user_manager.user_manager.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
+import java.util.Set;
 
 @Entity
+@Data
 @Table(name="users")
 public class UserModel {
     @Id
     @UuidGenerator
     private String uuid;
 
+    @Column(nullable = false, unique = true)
     private String userName;
 
+    @Column(nullable = false, unique = true)
     private String password;
 
-    public UserModel() {
-    }
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users-roles",
+            joinColumns = @JoinColumn(name="user_id", referencedColumnName = "uuid"),
+            inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "id")
+            )
+    private Set<Role> roles;
 
-    public String getUuid() {
-        return uuid;
-    }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
