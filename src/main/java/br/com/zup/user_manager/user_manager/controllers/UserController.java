3f42481c8368b7561992735eda8e5ac3a078b4cf
Eloiza <eloiza.souza@zup.com.br>
2025-02-21 09:57:17 -0300

    style: organize imports from UserController

--- src/main/java/br/com/zup/user_manager/user_manager/controllers/UserController.java
+++ src/main/java/br/com/zup/user_manager/user_manager/controllers/UserController.java
@@ -1,18 +1,14 @@
 package br.com.zup.user_manager.user_manager.controllers;
 
 import br.com.zup.user_manager.user_manager.models.UserModel;
-import br.com.zup.user_manager.user_manager.repositories.UserLoginDTO;
+import br.com.zup.user_manager.user_manager.dtos.UserLoginDTO;
 import br.com.zup.user_manager.user_manager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
